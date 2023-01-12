57466150b16d6ba5d3e5ce79c9e3caf2be911606
salarissa321 <salarissa321@gmail.com>
2023-01-12 21:18:38 +0100

    add 1 section

---
+++ Project 1/code.js
@@ -0,0 +1,410 @@
+
+
+
+
+// // variabels
+// // let name = 'Salar'
+// // let age = 20;
+
+// let Name = 'salar' ,  age = 25
+
+// let Salary = 1000;
+
+// if (age > 20) {
+//     console.log('Enter')
+// }
+
+// console.log (Name);
+// console.log (age);
+// console.log (Salary);
+
+
+
+// console.log("Hello, World!");
+
+
+// let name = ('salar')
+// let age = 20;
+
+
+// let Salary;
+
+
+// Salary = 100;
+
+
+// Salary = 1000;
+
+// console.log (name)
+// console.log (age)
+
+// const COLOR-RED = '#fefefe'
+// let age = 10;
+//  age = 100;
+// / code 
+//  age = 1000;
+
+
+// let age= 10;
+
+
+// let student-name = 'Salar' ;
+
+// let s-name = 
+
+
+// console.log (x)
+
+// let studentName = 'Salar'
+// let studenFirsttName = 'Salar'
+
+
+// coding : convetion
+
+// let age = 10; # number 
+
+// let name = 'salar'
+// let age = '10';
+
+
+// let name = 'salar' 
+
+
+// let fname = "salar"
+
+
+// let f = َmy name is ${name} َ
+
+
+// let q = "what's your name"
+
+// console.log (f)
+
+
+// bool
+
+// let married = false;
+// let graduated = true;
+// console.log (married)
+
+// let value = 6>5 ;
+
+// console.log (value)
+
+// interaction
+// let age = prompt('How Old Are you ?')
+// console.log (age)
+
+// let married = confirm('Are You married ?')
+// console.log (married)
+
+// alert('my name is salar')
+
+// let value = true;
+
+// // console.log (typeof value)
+
+
+// console.log (String(value))
+
+
+// console.log (9%4)
+// console.log (11%3)
+
+// conditions
+
+
+// let age = prompt ('Enter your Age')
+
+
+// if (age > 20 ){
+//     console.log ('ok')
+// }else{
+//     console.log ('issue')
+// }
+
+// let username = 'Salar'
+// let password = 12345
+
+//  let user_input = prompt('Enter Username')
+//  let password_input = prompt('Enter password')
+
+
+//  if ( user_input == username){
+//     console.log ('login successed')
+//  }else{
+//     console.log ('login faild')
+//  }
+
+
+// let grade = prompt('Enter Your Grade')
+
+// if (grade > 90){
+//     console.log ('A+')
+// }
+
+//  else if (grade > 80){
+//     console.log ('B+')
+// }
+
+//  else if (grade > 70){
+//     console.log ('C+')
+// }
+
+
+// let allowAccsess ;
+
+// let age = prompt('eENter your Age')
+
+// if (age > 20){
+//     allowAccsess = true ; 
+// } else {
+//     allowAccsess = false ; 
+// }
+
+// console.log(allowAccsess)
+
+// ternary operators
+
+
+
+// let age = prompt('Enter your Age')
+
+// let allowAccsess = (age > 20) ? true : false ;
+
+// console.log(allowAccsess)
+
+// let company = prompt('Enter your Company')
+
+// if (company=='google'){
+//     alert('good')
+// }else {
+//     alert('bad')
+// } 
+
+
+// let company = prompt('Enter your Company');
+//  (company=='google') ? alert('good') : alert('bad')
+
+
+    // logical and or / or|| // and &&
+
+    // let username = 'Salar'
+    // let password = 12345
+    
+    //  let user_input = prompt('Enter Username')
+    //  let password_input = prompt('Enter password')
+    
+    
+    //  if ( user_input == username || password_input ==password ){
+    //     console.log ('login successed')
+    //  }else{
+    //     console.log ('login faild')
+    //  }
+
+
+    // # loops
+    // let i = 1;
+
+    // while (i<100){
+    //     console.log (i)
+    //     i = i+1
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
